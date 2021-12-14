5d6b39b13f64f401aade1358e98ff36f9965b818
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-14 15:07:47 -0600

    complete user story 1

--- spec/features/studio/index_spec.rb
+++ spec/features/studio/index_spec.rb
@@ -1,6 +1,21 @@
-# Studio Index
-#
-# As a user,
-# When I visit the studio index page
-# I see a each studio's name and location
-# And underneath each studio, I see the titles of all of its movies.
+require 'rails_helper'
+
+RSpec.describe "Studio Index Page" do
+
+  it 'can list all of the studios and their movies' do
+
+    disney = Studio.create!(name: 'Walt Disney Pictures', location: 'Burbank')
+    paramount = Studio.create!(name: 'Paramount Pictures', location: 'Los Angeles')
+    dory = disney.movies.create!(title: 'Finding Dory', creation_year: 2016 , genre: 'Family' )
+    transformers = paramount.movies.create!(title: 'Transformers', creation_year: 2007 , genre: 'Action' )
+
+    visit "/studios"
+
+    expect(page).to have_content("Name: Walt Disney Pictures")
+    expect(page).to have_content("Location: Burbank")
+    expect(page).to have_content("Name: Paramount Pictures")
+    expect(page).to have_content("Location: Los Angeles")
+    expect(page).to have_content("Movies: Finding Dory")
+    expect(page).to have_content("Movies: Transformers")
+  end
+end
