e442cff6e25b9136a39ab139badd00f686c6163e
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-14 14:43:03 -0600

    Start IC

---
+++ spec/features/studio/index_spec.rb
@@ -0,0 +1,6 @@
+# Studio Index
+#
+# As a user,
+# When I visit the studio index page
+# I see a each studio's name and location
+# And underneath each studio, I see the titles of all of its movies.
