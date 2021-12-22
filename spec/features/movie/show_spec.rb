a902084cbda396d00bb08fb40483406739b15cd5
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-12-22 10:08:19 -0600

    edit movie show page and spec to use movie instance methods

--- spec/features/movie/show_spec.rb
+++ spec/features/movie/show_spec.rb
@@ -13,14 +13,10 @@ RSpec.describe "Movie Show Page" do
     jon = Actor.create!(name: "Jon Voight", age: 82 )
     ellen = Actor.create!(name: "Ellen DeGeneres", age: 63 )
 
-    MovieActor.create!(movie_id: transformers.id, actor_id: megan.id )
-    MovieActor.create!(movie_id: transformers.id, actor_id: shia.id )
-    MovieActor.create!(movie_id: transformers.id, actor_id: jon.id )
-    MovieActor.create!(movie_id: dory.id, actor_id: ellen.id )
-
     visit "/movies/#{transformers.id}"
 
     expect(page).to have_content('Transformers')
+    expect(page).to have_content(transformers.title)
     expect(page).to have_content(2007)
     expect(page).to have_content('Action')
     expect(page).to_not have_content('Finding Dory')
@@ -32,21 +28,27 @@ RSpec.describe "Movie Show Page" do
     paramount = Studio.create!(name: 'Paramount Pictures', location: 'Los Angeles')
 
     transformers = paramount.movies.create!(title: 'Transformers', creation_year: 2007 , genre: 'Action' )
+    gump = paramount.movies.create!(title: 'Forrest Gump', creation_year: 1994 , genre: 'Drama')
 
     jon = Actor.create!(name: "Jon Voight", age: 82 )
     megan = Actor.create!(name: "Megan Fox", age: 35)
     shia = Actor.create!(name: "Shia LaBeouf", age: 37)
+    tom = Actor.create!(name: "Tom Hanks", age: 65)
 
     MovieActor.create!(movie_id: transformers.id, actor_id: megan.id )
     MovieActor.create!(movie_id: transformers.id, actor_id: shia.id )
     MovieActor.create!(movie_id: transformers.id, actor_id: jon.id )
+    MovieActor.create!(movie_id: gump.id, actor_id: tom.id)
 
     visit "/movies/#{transformers.id}"
 
     expect(page).to have_content("Megan Fox")
     expect(page).to have_content("Shia LaBeouf")
     expect(page).to have_content("Jon Voight")
-  #wasn't sure how to test for order, need orderly??
+    expect(page).to_not have_content("Tom Hanks")
+    expect(megan.name).to appear_before(jon.name)
+    expect(shia.name).to appear_before(jon.name)
+    expect(jon.name).to_not appear_before(megan.name)
   end
 
   it 'can show the average age of all of its actors' do
@@ -65,73 +67,5 @@ RSpec.describe "Movie Show Page" do
     visit "/movies/#{transformers.id}"
 
     expect(page).to have_content("Average age of actors: 51")
-  end
-
-  it 'has a form to add an actor to the movie' do
-    paramount = Studio.create!(name: 'Paramount Pictures', location: 'Los Angeles')
-
-    transformers = paramount.movies.create!(title: 'Transformers', creation_year: 2007 , genre: 'Action' )
-
-    megan = Actor.create!(name: "Megan Fox", age: 35)
-    shia = Actor.create!(name: "Shia LaBeouf", age: 37)
-    jon = Actor.create!(name: "Jon Voight", age: 82 )
-
-    MovieActor.create!(movie_id: transformers.id, actor_id: megan.id )
-    MovieActor.create!(movie_id: transformers.id, actor_id: shia.id )
-    MovieActor.create!(movie_id: transformers.id, actor_id: jon.id )
-
-    visit "/movies/#{transformers.id}"
-    expect(page).to_not have_content("Tyrese")
-    expect(page).to have_content("Add an actor to this movie")
-  end
-
-  it 'can search for an actor' do
-    paramount = Studio.create!(name: 'Paramount Pictures', location: 'Los Angeles')
-
-    transformers = paramount.movies.create!(title: 'Transformers', creation_year: 2007 , genre: 'Action' )
-
-    megan = Actor.create!(name: "Megan Fox", age: 35)
-    shia = Actor.create!(name: "Shia LaBeouf", age: 37)
-    jon = Actor.create!(name: "Jon Voight", age: 82 )
-    tyrese = Actor.create!(name: "Tyrese", age: 42 )
-
-    MovieActor.create!(movie_id: transformers.id, actor_id: megan.id )
-    MovieActor.create!(movie_id: transformers.id, actor_id: shia.id )
-    MovieActor.create!(movie_id: transformers.id, actor_id: jon.id )
-    MovieActor.create!(movie_id: transformers.id, actor_id: tyrese.id )
-
-    visit "/movies/#{transformers.id}"
-
-    fill_in 'search', with:('Tyrese')
-    click_on("Search")
-
-    expect(current_path).to eq("/movies/#{transformers.id}")
-    expect(page).to have_content('Tyrese')
-  end
-
-  xit 'has a button to add the actor to the movie' do
-    paramount = Studio.create!(name: 'Paramount Pictures', location: 'Los Angeles')
-
-    transformers = paramount.movies.create!(title: 'Transformers', creation_year: 2007 , genre: 'Action' )
-
-    megan = Actor.create!(name: "Megan Fox", age: 35)
-    shia = Actor.create!(name: "Shia LaBeouf", age: 37)
-    jon = Actor.create!(name: "Jon Voight", age: 82 )
-    tyrese = Actor.create!(name: "Tyrese", age: 42 )
-
-    MovieActor.create!(movie_id: transformers.id, actor_id: tyrese.id )
-
-    visit "/movies/#{transformers.id}"
-
-    fill_in 'search', with: ('Tyrese')
-    click_on("Search")
-
-    expect(current_path).to eq("/movies/#{transformers.id}")
-    expect(page).to have_content('Tyrese')
-
-    click_on("Submit")
-
-    expect(current_path).to eq("/movies/#{transformers.id}")
-    expect(page).to have_content('Tyrese')
   end
 end
